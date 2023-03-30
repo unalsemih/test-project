b2780d3c5b69ac89abeefa4abbe8c395887ae5ab
= <=>
2023-03-30 16:54:50 +0300

    test fail

--- __tests/sum.test.js
+++ __tests/sum.test.js
@@ -5,7 +5,7 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('sum all numbers', () => {
-  expect(sumAllNumbers([1, 2, 3])).toBe(6);
+  expect(sumAllNumbers([1, 2, 3, 4])).toBe(6);
 });
 
 test('if parameter is not array, returns void', () => {
